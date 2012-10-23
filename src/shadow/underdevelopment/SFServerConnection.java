477476c1f85c6b1d3ca4e533c50456d1f5c8333e
Luigi Pasotti <luigi@acquarius.(none)>
2012-10-23 16:12:34 +0200

    Starting version of SFServerConnection.java and his status enumeration

--- src/shadow/underdevelopment/SFServerConnection.java
+++ src/shadow/underdevelopment/SFServerConnection.java
@@ -9,6 +9,12 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import shadow.system.data.SFInputStream;
+import shadow.system.data.SFOutputStream;
+import shadow.system.data.java.SFIOExceptionKeeper;
+import shadow.system.data.java.SFInputStreamJava;
+import shadow.system.data.java.SFOutputStreamJava;
+
 /**
  * @author Luigi Pasotti
  *
@@ -16,14 +22,16 @@ import java.net.Socket;
 public class SFServerConnection {
 	private ServerSocket serverSocket = null;
 	private Socket clientSocket = null;
+	private int port;
 	
-	public SFServerConnection(){
-		
+	public SFServerConnection(int port){
+		this.port = port;
 	}
 	
-	public void openConnections(int port) throws IOException{
+	//TODO: architettura di risposta asincrona
+	public void openConnections() throws IOException{
 		try{
-			serverSocket = new ServerSocket(port);
+			serverSocket = new ServerSocket(this.port);
 		} catch (IOException e) {
 			System.err.println("Could not listen on selected port.");
 			e.printStackTrace();
@@ -39,8 +47,19 @@ public class SFServerConnection {
 	}
 	
 	public void closeConnection() throws IOException {
-		clientSocket.close();
-		serverSocket.close();
+		try {
+			clientSocket.close();
+			serverSocket.close();
+		} catch (IOException e) {
+			System.err.println("An I/O error occurs when closing this socket.");
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	public SFServerConnectionStatus getStatus(){
+		//TODO: do a serious check on the connection status
+		return SFServerConnectionStatus.SOCKET_CONNECTED;
 	}
 	
 	public InputStream getInputStream() throws IOException {
@@ -55,12 +74,57 @@ public class SFServerConnection {
 		return input;
 	}
 	
+	public SFInputStream getSFInputStream() throws IOException{
+		SFInputStream input = null;
+		try {
+			if (this.clientSocket!=null) {
+				InputStream stream = this.clientSocket.getInputStream();
+				input = new SFInputStreamJava(stream, new SFIOExceptionKeeper() {
+					
+					@Override
+					public void launch(IOException exception) {
+						System.err.println("Couldn't create an SFInputStream from ImputStream.");
+						exception.printStackTrace();
+					}
+				});
+			}
+		} catch (IOException e) {
+			System.err.println("Couldn't get an InputStream from socket.");
+			e.printStackTrace();
+			throw e;
+		}
+		return input;
+	}
+	
+	
 	public OutputStream getOutputStream() throws IOException{
 		OutputStream output = null;
 		try {
 			output = this.clientSocket.getOutputStream();
 		} catch (IOException e) {
 			System.err.println("Couldn't get an OutputStream from socket.");
+			e.printStackTrace();
+			throw e;
+		}
+		return output;
+	}
+	
+	public SFOutputStream getSFOutputStream() throws IOException{
+		SFOutputStream output = null;
+		try {
+			if (this.clientSocket!=null) {
+				OutputStream stream = this.clientSocket.getOutputStream();
+				output = new SFOutputStreamJava(stream, new SFIOExceptionKeeper() {
+					
+					@Override
+					public void launch(IOException exception) {
+						System.err.println("Couldn't create an SFInputStream from ImputStream.");
+						exception.printStackTrace();
+					}
+				});
+			}
+		} catch (IOException e) {
+			System.err.println("Couldn't get an InputStream from socket.");
 			e.printStackTrace();
 			throw e;
 		}
---
+++ src/shadow/underdevelopment/SFServerConnectionStatus.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package shadow.underdevelopment;
+
+/**
+ * @author Luigi Pasotti
+ *
+ */
+public enum SFServerConnectionStatus {
+	SOCKET_NULL,
+	SOCKET_CONNECTED,
+	SOCKET_NOT_CONNECTED;
+}
