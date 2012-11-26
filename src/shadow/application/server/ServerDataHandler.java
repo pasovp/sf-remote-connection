ab96d73db49eec20b478e8dee20617c44e2595a6
Luigi Pasotti <luigi@acquarius.(none)>
2012-11-26 15:10:08 +0100

    Added the default assets library xml file
    ServerDataHandler.java updated for loading default data.

--- src/shadow/application/server/ServerDataHandler.java
+++ src/shadow/application/server/ServerDataHandler.java
@@ -3,9 +3,12 @@
  */
 package shadow.application.server;
 
+import shadow.renderer.data.java.SFObjectsLibraryDecoder;
+import shadow.renderer.data.java.SFXMLDataInterpreter;
 import shadow.renderer.data.utils.SFDataUtility;
 import shadow.system.data.SFDataset;
 import shadow.system.data.SFObjectsLibrary;
+import shadow.system.data.SFObjectsLibrary.RecordData;
 
 /**
  * @author Luigi Pasotti
@@ -25,9 +28,23 @@ public class ServerDataHandler implements InterfServerDataLibrary {
 		this.library = new SFObjectsLibrary();
 	}
 	
-	public void loadLibrary() {		
-		SFDataset dataset = SFDataUtility.loadDataset(ROOT, FILENAME + ".sf");
-		this.library = (SFObjectsLibrary)(dataset);
+	public void loadLibrary() {
+		SFObjectsLibrary lib;
+		lib = (SFObjectsLibrary) SFDataUtility.loadDataset(ROOT, FILENAME + ".sf");
+		this.library = lib;
+		
+		lib = (SFObjectsLibrary) SFDataUtility.loadDataset(ROOT, "default.sf");
+		if(lib == null){
+			lib = new SFObjectsLibrary();
+			SFObjectsLibraryDecoder decoder = new SFObjectsLibraryDecoder(lib);
+			SFXMLDataInterpreter interpreter = new SFXMLDataInterpreter(decoder);
+			interpreter.generateInterpretation(ROOT + "/default.xml" );
+			SFDataUtility.saveDataset(ROOT, "default.sf", lib);
+		} 
+		for (RecordData recordData : lib) {
+			this.library.put(recordData);
+		}
+		
 	}
 
 
