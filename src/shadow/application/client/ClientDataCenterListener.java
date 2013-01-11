56571687f377fbc3e5f24e3f34498a7fd914ebc9
Luigi Pasotti <pasovp@gmail.com>
2013-01-11 16:35:23 +0100

    ClientDataCenter fixed
    Now only one window is opened by the application.
    The window color controller and rotation controller didn't work
    properly, I suspect that other controllers may have problems too.

--- src/shadow/application/client/ClientDataCenterListener.java
+++ src/shadow/application/client/ClientDataCenterListener.java
@@ -3,66 +3,41 @@ package shadow.application.client;
 import shadow.renderer.SFObjectModel;
 import shadow.renderer.contents.tests.common.CommonMaterial;
 import shadow.renderer.data.SFObjectModelData;
-import shadow.renderer.viewer.SFDrawableFrame;
 import shadow.renderer.viewer.SFViewer;
 import shadow.system.data.SFDataCenterListener;
 
 public class ClientDataCenterListener<T extends SFObjectModelData> implements SFDataCenterListener<SFObjectModelData> {
-	private SFObjectModel mainNode = new SFObjectModel();
+	//private SFObjectModel mainNode = new SFObjectModel();
 	private boolean windowOpened = false;
 	private SFViewer viewer;
+	//private static int counter;
 	
 	public ClientDataCenterListener() {
-		//viewer = new SFViewer();
 	}
 	
 	@Override
-	public void onDatasetAvailable(String name, SFObjectModelData dataset) {
-//		SFObjectModel model=(SFObjectModel)dataset.getResource();
-//		mainNode.setModel(model.getModel());
-//		
-//		if(windowOpened == false) {
-//			SFViewer.generateFrame(mainNode,
-//				CommonMaterial.generateColoursController(mainNode),
-//				SFViewer.getLightStepController(),
-//				SFViewer.getRotationController(),
-//				SFViewer.getWireframeController(),
-//				SFViewer.getZoomController());
-//			windowOpened = true;
-//		}
-		
+	public synchronized void onDatasetAvailable(String name, SFObjectModelData dataset) {
 		SFObjectModel model=(SFObjectModel)dataset.getResource();
-		//mainNode.setModel(model.getModel());
 		
-		if(windowOpened == false) {
-			viewer = SFViewer.generateFrame(model,
-				CommonMaterial.generateColoursController(model),
-				SFViewer.getLightStepController(),
-				SFViewer.getRotationController(),
-				SFViewer.getWireframeController(),
-				SFViewer.getZoomController());
-//			viewer.getFrame().dispose();
-			windowOpened = true;
-		} else {
-			//viewer.setNode(model);
-//			viewer.getFrame().setVisible(false);
-//			viewer.getFrame().dispose();
-//			viewer = SFViewer.generateFrame(model,
-//					CommonMaterial.generateColoursController(model),
-//					SFViewer.getLightStepController(),
-//					SFViewer.getRotationController(),
-//					SFViewer.getWireframeController(),
-//					SFViewer.getZoomController());
-			viewer.getFrame().setVisible(false);
-			//viewer.getFrame().dispose();
-			viewer.setNode(model);
-			viewer.setFrame(new SFDrawableFrame("Scene Viewer", 600, 600, viewer,
+		try {
+			if(windowOpened == false) {
+				viewer = SFViewer.generateFrame(model,
 					CommonMaterial.generateColoursController(model),
 					SFViewer.getLightStepController(),
 					SFViewer.getRotationController(),
 					SFViewer.getWireframeController(),
-					SFViewer.getZoomController()));
-			viewer.getFrame().setVisible(true);
+					SFViewer.getZoomController());
+//				counter++;
+//				if(counter>=2){
+//					throw new RuntimeException("2 finestre");
+//				}
+				windowOpened = true;
+			} else {
+				viewer.setNode(model);
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
 		}
 	}
 }
