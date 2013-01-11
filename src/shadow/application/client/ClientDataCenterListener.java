b5b5698d25345c8d22f5691c5d0bc5a378988871
Luigi Pasotti <pasovp@gmail.com>
2013-01-11 14:13:20 +0100

    Test

--- src/shadow/application/client/ClientDataCenterListener.java
+++ src/shadow/application/client/ClientDataCenterListener.java
@@ -3,11 +3,11 @@ package shadow.application.client;
 import shadow.renderer.SFObjectModel;
 import shadow.renderer.contents.tests.common.CommonMaterial;
 import shadow.renderer.data.SFObjectModelData;
+import shadow.renderer.viewer.SFDrawableFrame;
 import shadow.renderer.viewer.SFViewer;
-import shadow.system.data.wip.SFUpdatableDatasetListener;
+import shadow.system.data.SFDataCenterListener;
 
-public class ClientDataCenterListener<T extends SFObjectModelData> implements SFUpdatableDatasetListener<SFObjectModelData> {
-	
+public class ClientDataCenterListener<T extends SFObjectModelData> implements SFDataCenterListener<SFObjectModelData> {
 	private SFObjectModel mainNode = new SFObjectModel();
 	private boolean windowOpened = false;
 	private SFViewer viewer;
@@ -41,24 +41,28 @@ public class ClientDataCenterListener<T extends SFObjectModelData> implements SF
 				SFViewer.getRotationController(),
 				SFViewer.getWireframeController(),
 				SFViewer.getZoomController());
+//			viewer.getFrame().dispose();
 			windowOpened = true;
 		} else {
+			//viewer.setNode(model);
+//			viewer.getFrame().setVisible(false);
+//			viewer.getFrame().dispose();
+//			viewer = SFViewer.generateFrame(model,
+//					CommonMaterial.generateColoursController(model),
+//					SFViewer.getLightStepController(),
+//					SFViewer.getRotationController(),
+//					SFViewer.getWireframeController(),
+//					SFViewer.getZoomController());
+			viewer.getFrame().setVisible(false);
+			//viewer.getFrame().dispose();
 			viewer.setNode(model);
+			viewer.setFrame(new SFDrawableFrame("Scene Viewer", 600, 600, viewer,
+					CommonMaterial.generateColoursController(model),
+					SFViewer.getLightStepController(),
+					SFViewer.getRotationController(),
+					SFViewer.getWireframeController(),
+					SFViewer.getZoomController()));
+			viewer.getFrame().setVisible(true);
 		}
-	}
-	
-	@Override
-	public void onDatasetUpdate(String name, SFObjectModelData dataset) {
-		
-//		SFObjectModel model=(SFObjectModel)dataset.getResource();
-//		mainNode.setModel(model.getModel());
-//		
-//		SFViewer.generateFrame(mainNode,
-//				CommonMaterial.generateColoursController(mainNode),
-//				SFViewer.getLightStepController(),
-//				SFViewer.getRotationController(),
-//				SFViewer.getWireframeController(),
-//				SFViewer.getZoomController());
-		
 	}
 }
