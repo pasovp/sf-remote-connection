f7213098a4fa98e340c33c5db8386d8ec1a35d34
Luigi Pasotti <pasovp@gmail.com>
2013-03-06 17:00:11 +0100

    New type SFDatasetReplacement and tests for load/store in xml files. This class is designed to substitute SFDefaultDatasetReference in a ObjectLibrary.

---
+++ src/shadow/application/server/test/DatasetReplacementTest_01.java
@@ -0,0 +1,32 @@
+package shadow.application.server.test;
+
+import shadow.renderer.data.utils.SFDataUtility;
+import shadow.renderer.data.utils.SFViewerDatasetFactory;
+import shadow.system.data.SFDataCenter;
+import shadow.system.data.SFObjectsLibrary;
+import shadow.system.data.wip.SFDatasetReplacement;
+
+public class DatasetReplacementTest_01 {
+	
+	private static SFObjectsLibrary defaultReplacementsLibrary;
+	private static final String ROOT = "appdata/replacement_tests";
+	private static final String FILENAME = "replacement_test01";
+	
+	/** 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SFViewerDatasetFactory factory = new SFViewerDatasetFactory();
+		factory.addSFDataset(new SFDatasetReplacement());
+		SFDataCenter.setDatasetFactory(factory);
+		
+		// generating a new library with one SFDatasetReplacement
+		defaultReplacementsLibrary = new SFObjectsLibrary();
+		SFDatasetReplacement replacement = new SFDatasetReplacement("Mushroom", System.currentTimeMillis());
+		defaultReplacementsLibrary.put("RedMushroom", replacement);
+		
+		// store the library in a xml file
+		SFDataUtility.saveXMLFile(ROOT, FILENAME, defaultReplacementsLibrary);
+	}
+
+}
---
+++ src/shadow/system/data/wip/SFDatasetReplacement.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package shadow.system.data.wip;
+
+import shadow.renderer.data.SFAssetModule;
+import shadow.system.data.SFNamedParametersObject;
+import shadow.system.data.object.wip.SFLong;
+import shadow.system.data.objects.SFString;
+
+/**
+ * @author luigi
+ *
+ */
+public class SFDatasetReplacement extends SFAssetModule {
+	
+	private SFString replacement = new SFString("");
+	private SFLong timestamp = new SFLong(0);
+	
+	public SFDatasetReplacement() {
+		SFNamedParametersObject parameters=new SFNamedParametersObject();
+		parameters.addObject("replacement", this.replacement);
+		parameters.addObject("timestamp", this.timestamp);
+		setData(parameters);
+	}
+	
+	public SFDatasetReplacement(String replacement, Long timestamp) {
+		this();
+		this.setReplacement(replacement, timestamp);
+		
+	}
+	
+	public void setReplacement(String replacement, Long timestamp) {
+		this.replacement.setString(replacement);
+		this.timestamp.setLongValue(timestamp);
+	}
+
+}
